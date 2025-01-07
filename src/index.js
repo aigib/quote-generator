9619ba2f25e66c185cc52e70d53174eb89980f5f
aigib <aigibuht@gmail.com>
2025-01-07 13:01:05 +0200

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "You have within you, right now",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
